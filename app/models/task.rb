e32dbdbc61bfb024dc4e6f9012498926a605c32e
Kevin <contact@kevinbongart.net>
2011-09-08 02:30:31 -0400

    Inbox count only when > 0

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ActiveRecord::Base
   belongs_to :project
   belongs_to :zone
 
-  scope :inbox, where("tasks.project_id IS NULL OR tasks.zone_id IS NULL")
+  scope :inbox, where("tasks.project_id IS NULL OR tasks.zone_id IS NULL").where(:done => false)
   default_scope order("done, due_date ASC")
 end
